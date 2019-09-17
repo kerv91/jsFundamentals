aec79143937bd9a3c60ddb61b1f6f9c8f7a5fbaf
kerv91 <kerv@att.net>
2019-09-17 11:06:41 -0400

    scope, hoist,literals

---
+++ 01-Grammar&Types/06-literals.js
@@ -0,0 +1,24 @@
+//STRING LITERAL 
+let car = 'Ford';
+
+//NUMERIC LITERAL 
+let december = 12;
+
+//BOOLEAN LITERAL 
+let tired = true;
+
+//OBJECT LITERAL
+let soup = {
+    a: 'chicken noodle',
+    b: 'tomato',
+    c: 'beef and barley'
+}
+
+console.log(soup.c);
+
+//ARRAY LITERALS
+let days = ['Monday', 'Tuesday', 'Wednesday','Thursday','Friday'];
+
+console.log(days.length);
+
+console.log(days.toString())
